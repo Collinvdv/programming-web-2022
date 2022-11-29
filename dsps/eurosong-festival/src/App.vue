2f1a0f28d88739ced6eb16239199c92a4da48b31
Collin Van der Vorst <collin@littlemissrobot.com>
2022-11-29 09:32:52 +0100

    Card

--- dsps/eurosong-festival/src/App.vue
+++ dsps/eurosong-festival/src/App.vue
@@ -3,6 +3,15 @@
     <h1>
       Playground vue.js
     </h1>
+
+    <Card title="test 1" showButtons link="https://youtube.com"/>
+
+    <Card title="test 2" />
+
+    <Card title="test 3" showButtons isButtonsDisabled/>
+
+    <Card title="test 4"/>
+
   </div>
 </template>
 
--- dsps/eurosong-festival/src/components/Card.vue
+++ dsps/eurosong-festival/src/components/Card.vue
@@ -1,17 +1,78 @@
 <template>
   <div>
     <h2>
-      Card title
+      {{ title.toUpperCase() }}
     </h2>
+
+    <p>
+      Counter with number of <span> {{ counter }}</span>
+    </p>
+
+    <p v-html="welcome">
+    </p>
+
+    <ul>
+      <li v-for="(user, index) in users" :key="index">
+        {{ user }}
+      </li>
+    </ul>
+
+    <ul>
+      <li v-for="(user, index) in users" :key="index">
+        {{ user }}
+      </li>
+    </ul>
+
+    <div v-if="showButtons">
+      <button @click="addOne()" :disabled="isButtonsDisabled">
+        Plus one
+      </button>
+      
+      <button @click="minusOne()" :disabled="isButtonsDisabled">
+        Minus one
+      </button>
+    </div>
+
+    <a :href="link" v-if="link">
+      Visit the website
+    </a>
   </div>
 </template>
 
 <script>
-  export default {
+export default {
+    // The name of the component
     name: 'Card',
-    // props: {
-    //   msg: String
-    // }
+    // Properties from outside of component
+    props: {
+      title: String,
+      showButtons: Boolean,
+      isButtonsDisabled: Boolean,
+      link: String
+    },
+    // Data is for internal use
+    data() {
+      return {
+        counter: 0,
+        welcome: "Hello <strong> user</strong>",
+        users: [
+          "Collin",
+          "Elke",
+          "Tristan"
+        ]
+      }
+    },
+    // Functionalities connected with template
+    methods: {
+      addOne() {
+        // Why is it possible to change properties???? @todo -> announcement 
+        this.counter++;
+      },
+      minusOne() {
+        this.counter--;
+      },
+    },
+    
   }
 </script>
 
