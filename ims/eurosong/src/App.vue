30a01d962939b46380bc7efaa9464876c3df9d2a
Collin Van der Vorst <collin@littlemissrobot.com>
2022-12-20 13:58:45 +0100

    Add binding

--- ims/eurosong/src/App.vue
+++ ims/eurosong/src/App.vue
@@ -22,7 +22,7 @@ export default {
   name: "App",
   data() {
     return {
-      activePage: "voting",
+      activePage: "home",
       pages: [
         "home",
         "ranking",
--- ims/eurosong/src/pages/PageHome.vue
+++ ims/eurosong/src/pages/PageHome.vue
@@ -3,11 +3,45 @@
     <h1>
         PageHome
     </h1>
+
+    <input type="text" v-model="favoriteCountry" />
+    <button @click="screamCountry()">
+        Scream favorite country
+    </button>
    </div>
 </template>
 
 <script>
 export default {
     name: 'PageHome',
+    mounted() {
+        // functie wordt automatisch uitgevoerd van zodra het comp
+        // zichtbaar is 
+        fetch("http://webservies.be/eurosong/Votes", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json, text/plain',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify({
+                songID: this.songs[this.activeSongIndex].id,
+                points: this.voteButtons[buttonIndex].points
+            })
+        }).then((response) => {
+            return response.json()
+        }).then((result) => {
+            console.log(result);
+        })
+    },
+    data() {
+        return {
+            favoriteCountry: ""
+        }
+    },
+    methods: {
+        screamCountry() {
+            alert(this.favoriteCountry);
+        }
+    }
 }
 </script>
