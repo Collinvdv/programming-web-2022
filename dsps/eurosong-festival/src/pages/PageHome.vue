e3a19ee609d16cdb90008036197ac655e67264c3
Collin Van der Vorst <collin@littlemissrobot.com>
2022-12-20 13:00:19 +0100

    Page home

--- dsps/eurosong-festival/src/pages/PageHome.vue
+++ dsps/eurosong-festival/src/pages/PageHome.vue
@@ -1,12 +1,95 @@
 <template>
    <div>
-    Page Home
+        Page Home
+        <br>
+        <input type="text" v-model="newArtist">
+        {{  newArtist }}
+        <button @click="addArtist()">
+            Add Artist
+        </button>
+        <hr>
+        <table border="1">
+            <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Actions
+                </th>
+            </tr>
+
+            <tr v-for="(artist , index) in artists" :key="index">
+                <td>
+                    {{  artist.id }}
+                </td>
+                <td>
+                    {{ artist.name }}
+                </td>
+                <td>
+                    <button @click="deleteArtist(artist.id)">
+                        Delete the artist
+                    </button>
+                </td>
+            </tr>
+        </table>
+
    </div>
 </template>
 
 <script>
 export default {
     // The name of the component
-    name: 'PageHome'
+    name: 'PageHome',
+    mounted() { 
+        this.fetchArtists();
+    },
+    data() {
+        return {
+            artists: [],
+            newArtist: ""
+        }
+    },
+    methods: {
+        deleteArtist(id) { 
+            fetch(`http://webservies.be/eurosong/Artists?id=${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Accept': 'application/json, text/plain',
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+            }).then((response) => {
+                return response.json()
+            }).then(() => {
+                this.fetchArtists();
+            });
+        },
+        addArtist() { 
+            fetch("http://webservies.be/eurosong/Artists", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json, text/plain',
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+                body: JSON.stringify({
+                    name: this.newArtist
+                })
+            }).then((response) => {
+                return response.json()
+            }).then((result) => {
+                this.fetchArtists();
+                console.log(result);
+            })
+        },
+        fetchArtists() { 
+            fetch('http://webservies.be/eurosong/Artists')
+                .then((response) => response.json())
+                .then((artists) => {
+                    this.artists = artists;
+                });
+        }
+    }
 }
 </script>
